0f6e552f1ace927dfe71cd71d2173ffbb2582642
chitachiamaka <chitachiamaka.github@gmail.com>
2022-04-25 11:55:58 +0100

    love commit

---
+++ App.js
@@ -0,0 +1,18 @@
+var msg = document.getElementById("msg");
+function lovCalc(){
+    var fName = prompt('Enter your name');
+    var lName = prompt("Enter your partner's name");
+    var xName = fName.slice(0,1);
+    var yName = xName.toUpperCase();
+    var zName = yName + fName.slice(1);
+    var aName = lName.slice(0,1);
+    var bName = aName.toUpperCase();
+    var cName = bName + lName.slice(1);
+    var love = Math.random();
+    love = love * 100;
+    love = Math.floor(love) + 1;
+    var output = zName + " and " + cName + " are " + love + "% match!!!";
+      return output;
+}
+var match = lovCalc();
+msg.innerHTML = match;
